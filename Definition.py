951f8a50586ed55369a983a995de08f53599765d
Afmr1124 <108682287+Afmr1124@users.noreply.github.com>
2022-07-16 17:26:56 +0800

    用一檔抵全部
    去問了號爾助教
    他很快就回覆我了
    我的問題給他
    他馬上就幫我解答
    而且很清楚
    還多教我其他的方法，還有效率問題
    還給了我一個網站
    w3school.com
    學完可以寫馬爾地夫了喔(XD

--- Definition.py
+++ Definition.py
@@ -1,3 +1,7 @@
+from configparser import LegacyInterpolation
+import linecache
+
+
 print('「定義」(代碼如下): ')
 print(' 一分人 = 1 ')
 print(' 二分人 = 2 ')
@@ -5,5 +9,40 @@ print(' 三分人 = 3 ')
 print(' 四分人 = 4 ')
 print(' 無定義 = 5 ')
 definition=input('請輸入您的定義代碼 = ')
+
+Definition=open("C:\\Users\\Owner\\Documents\\GitHub\\-\\Definition_txt\\All definition.txt",encoding="utf-8")
+line = Definition.readlines()
+
 if definition=="1":
-    595
+    for i in range(0,11,1):
+        print(line[i])
+elif definition=="2":
+    for i in range(12,31,1):
+        print(line[i])
+elif definition=="3":
+    for i in range(32,48,1):
+        print(line[i])
+elif definition=="4":
+    for i in range(49,60,1):
+        print(line[i])
+elif definition=="5":
+    for i in range(61,63,1):
+        print(line[i])
+else:
+    print('代碼錯誤')
+for i in range(63,74,1):
+    print(line[i])
+
+
+        
+        
+
+
+
+    '''
+    for i in range(0,8,1):
+        S1=linecache.getlines('C:\\Users\\Owner\\Documents\\GitHub\\-\\Definition_txt\\All definition.txt',i)
+        S1=S1
+        print(S1)
+    Definition.close
+    '''
---
+++ Profile.py
@@ -0,0 +1,6 @@
+from Definition import Definition
+
+
+for i in range(0,15,1):
+    line=Definition.read()[i]
+    print(line)
