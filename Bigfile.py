5f9cccfef3d384f04503aa4fa4629632b7778b74
Afmr1124 <108682287+Afmr1124@users.noreply.github.com>
2022-07-17 23:47:02 +0800

    九大中心完成~~
    累類累累累
    0.0
    要找人來幫忙嗎??
    有誰呢??
    會有誰那麼鹹
    明天先用多元吧OUO

--- Bigfile.py
+++ Bigfile.py
@@ -1,11 +1,11 @@
-import sqlite3
-conn = sqlite3.connect('D.db')
-
-cursor = conn.cursor()
-cursor.execute("DROP TABLE  IF EXISTS  away;")
-cursor.execute('DROP TABLE  IF EXISTS  home;')
-
-print('D.db created successfully')
-conn.commit()
-cursor.close()
-conn.close()
+Center=open('README_for_start\LKK.txt','r',encoding="UTF-8")
+line=Center.readlines()
+for i in range(0,63,1):
+    print(line[i])
+Center.close()
+center=input('請輸入你想看的中心代碼= ')
+if center=='A':
+    for i in range(67,79):
+        print(line[i])
+else:
+    print('tahh')
