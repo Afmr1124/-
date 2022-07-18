4e9ce02d143386b0eb363e816b8093c0aea8d8e0
Afmr1124 <108682287+Afmr1124@users.noreply.github.com>
2022-07-18 22:10:07 +0800

    痾痾痾
    我找到了一個網站
    humandesign plus
    他分析了很多
    但都很文言
    所以我要繼續弄嗎??
    要的
    !!!!!!!

--- Profile.py
+++
@@ -1,6 +0,0 @@
-from Definition import Definition
-
-
-for i in range(0,15,1):
-    line=Definition.read()[i]
-    print(line)
---
+++ Sgal.py
@@ -0,0 +1,18 @@
+'''
+人類圖
+ - 類型(Type)
+    - 策略
+    - 非自己主題
+ - 人生腳色(Profile)
+ - 定義(Definition)
+ - 內在權威(Inner authority)
+    - 情緒型(Emotional-Solar Plexus)
+    - 薦骨型(Sacral)
+    - 直覺型(Splenic)
+    - 意志力型(Ego)
+    - 自我投射型(Self-Projected)
+    - 無內在權威(None)
+ - 通道(Channel)*36
+ - 閘門(Gate)*64
+ - 輪迴交叉(Incarnation Cross)
+'''
